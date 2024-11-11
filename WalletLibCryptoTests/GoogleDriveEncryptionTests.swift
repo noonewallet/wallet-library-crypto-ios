4d867e90ec74de2a0428b27932bfd7dd83ca70e8
Alek Mur <alek.mur@noone.io>
2024-11-11 16:44:52 +0100

    Update project file

---
+++ WalletLibCryptoTests/GoogleDriveEncryptionTests.swift
@@ -0,0 +1,16 @@
+//
+//  GoogleDriveEncryption.swift
+//  WalletLibCryptoTests
+//
+//  Created by Aleksei Muraveinik on 7.11.24..
+//
+
+import Testing
+
+struct GoogleDriveEncryption {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
