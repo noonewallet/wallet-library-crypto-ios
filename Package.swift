276b86bb5284739408ff04f4d4389964ba55218f
Peter Lavskiy <p.lavskiy.dev@gmail.com>
2025-04-29 13:34:28 -0300

    Add sources , excludes and paths to manifest

---
+++ Package.swift
@@ -0,0 +1,59 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "WalletLibCrypto",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "WalletLibCrypto",
+            targets: ["WalletLibCrypto"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "libcrypto",
+            path: "WalletLibCrypto/OpenSSL/libcrypto.xcframework"
+        ),
+        .target(
+            name: "WalletLibCryptoC",
+            path: "WalletLibCrypto",
+            sources: [
+                "WalletLibCrypto/Hash/Keccak/keccak-tiny.c",
+                "WalletLibCrypto/Hash/Blake2b/blake2b.c",
+                "WalletLibCrypto/Ed25519/ed25519.c",
+                "WalletLibCrypto/EdDSA/TweetNaCl.c"
+            ],
+            publicHeadersPath: "PrivateHeaders",
+            cSettings: [
+                .headerSearchPath("PrivateHeaders"),
+                .headerSearchPath("Hash/Keccak"),
+                .headerSearchPath("Hash/Blake2b"),
+                .headerSearchPath("Ed25519"),
+                .headerSearchPath("EdDSA")
+            ]
+        ),
+        .target(
+            name: "WalletLibCrypto",
+            dependencies: ["WalletLibCryptoC", "libcrypto"],
+            path: "WalletLibCrypto",
+            exclude: ["Scripts", "OpenSSL"],
+            sources: [
+                "Extensions",
+                "Encoding",
+                "Crypto/PBKDF2SHA512",
+                "Crypto/Signature",
+                "Crypto/HDDerivation"
+            ]
+        ),
+        .testTarget(
+            name: "WalletLibCryptoTests",
+            dependencies: ["WalletLibCrypto"],
+            path: "WalletLibCryptoTests"
+        )
+    ]
+)
+
